a13b571e05d92647202aab0a99ace83fb68bbfd9
Oskar <oskar.maslek@gmail.com>
2020-01-11 19:27:45 +0100

    Init app and import base packages

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
+import "bootstrap/dist/css/bootstrap.css";
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return 'App';
+    }
+}
+
+export default App;
