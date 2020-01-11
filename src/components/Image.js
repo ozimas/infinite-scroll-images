a301ce246239c0cef22723f1bda7c6cd37a8c071
Oskar <oskar.maslek@gmail.com>
2020-01-11 20:09:22 +0100

    Add preloader to images and add animation

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,19 +1,35 @@
 import React from "react";
+import "./Image.css";
+import Loader from "./Loader";
 
 class Image extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      loaded: false
+    };
+
+    this.isLoaded = this.isLoaded.bind(this);
+  }
+  isLoaded() {
+    this.setState({
+      loaded: true
+    });
   }
   render() {
     return (
       <div className="single-dog">
+        {this.state.loaded ? "" : <Loader />}
         <img
-          className={"single-dog__image"}
+          className={`single-dog__image rounded ${
+            this.state.loaded ? "load-image" : "unload-image"
+          }`}
           src={this.props.photo.src}
           width={this.props.photo.width}
           height={this.props.photo.height}
-          style={{margin: this.props.margin}}
+          style={{ margin: this.props.margin }}
           alt={"Image dog " + this.props.index}
+          onLoad={this.isLoaded}
         />
       </div>
     );
