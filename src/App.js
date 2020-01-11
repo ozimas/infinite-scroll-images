5456176170409ef94f40046534143ceedaaeec4b
Oskar <oskar.maslek@gmail.com>
2020-01-11 19:35:24 +0100

    Fetch images and add proxy to api

--- src/App.js
+++ src/App.js
@@ -6,7 +6,28 @@ import "bootstrap/dist/css/bootstrap.css";
 class App extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            photos: [],
+            api_url: "/api/shibes?count=16"
+        };
     }
+
+    componentDidMount() {
+        this.fetchImages();
+    }
+
+    fetchImages() {
+        axios.get(this.state.api_url).then(res => {
+          this.setState({
+            photos: this.state.photos.concat(
+              res.data.map(item_url => {
+                return {src: item_url, width: 1, height: 1};
+              })
+            )
+          });
+        });
+      }
+
     render() {
         return 'App';
     }
