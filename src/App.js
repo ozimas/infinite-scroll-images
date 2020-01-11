77d6aa9088cc6126e690559ab8dcfe0ff74f0464
Oskar <oskar.maslek@gmail.com>
2020-01-11 19:47:35 +0100

    Add function infinite scroll with images from api

--- src/App.js
+++ src/App.js
@@ -4,33 +4,43 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import "bootstrap/dist/css/bootstrap.css";
 
 class App extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            photos: [],
-            api_url: "/api/shibes?count=16"
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: [],
+      api_url: "/api/shibes?count=16"
+    };
+  }
 
-    componentDidMount() {
-        this.fetchImages();
-    }
+  componentDidMount() {
+    this.fetchImages();
+  }
 
-    fetchImages() {
-        axios.get(this.state.api_url).then(res => {
-          this.setState({
-            photos: this.state.photos.concat(
-              res.data.map(item_url => {
-                return {src: item_url, width: 1, height: 1};
-              })
-            )
-          });
-        });
-      }
+  fetchImages() {
+    axios.get(this.state.api_url).then(res => {
+      this.setState({
+        photos: this.state.photos.concat(res.data)
+      });
+    });
+  }
 
-    render() {
-        return 'App';
-    }
+  render() {
+    return (
+      <div className="container">
+        <InfiniteScroll
+          dataLength={this.state.photos.length}
+          scrollThreshold="200px"
+          next={this.fetchImages}
+          hasMore={true}
+          loader={"Loading..."}
+        >
+          {this.state.photos.map(item=>{
+              return <img src={item} />
+          })}
+        </InfiniteScroll>
+      </div>
+    );
+  }
 }
 
 export default App;
